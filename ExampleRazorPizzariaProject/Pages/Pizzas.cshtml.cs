68fcc2ecddf669ef48d70db7c4a8a1ee2b11c5ca
Mariana Casarin <mariana.battistin@gmail.com>
2023-05-30 13:31:08 +0100

    Implementation of the list of pizzas.

--- ExampleRazorPizzariaProject/Pages/Pizzas.cshtml.cs
+++ ExampleRazorPizzariaProject/Pages/Pizzas.cshtml.cs
@@ -1,3 +1,5 @@
+using ExampleRazorPizzariaProject.Data;
+using ExampleRazorPizzariaProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +7,20 @@ namespace ExampleRazorPizzariaProject.Pages
 {
     public class PizzasModel : PageModel
     {
-        public void OnGet()
-        {
+        [BindProperty]
+        public List<PizzaModel> Pizzas { get; set; }
 
+        private IDataRepository _repository;
+
+        public PizzasModel(IDataRepository repository)
+        {
+            _repository = repository;
+        }   
+
+        public async Task OnGetAsync()
+        {
+            var query = "select * from pizza order by Name";
+            Pizzas = await _repository.ListAsync<PizzaModel>(query);
         }
     }
 }
